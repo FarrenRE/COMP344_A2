7de808585020f437b0d902f656e466a52bbbd90e
FarrenRE <farren.esman@gmail.com>
2019-10-21 22:44:06 +1100

    responsive nav

--- index.php
+++ index.php
@@ -11,12 +11,17 @@
 <body>
   <header class="bg-dark">
     <section class="container">
-      <nav class="navbar navbar-dark header-nav">
-        <ul class="navbar-nav">
-          <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-          <li class="nav-item active"><a class="nav-link" href="#">Browse Products</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Compare</a></li>
-        </ul>
+      <nav class="navbar navbar-expand-sm navbar-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+            <li class="nav-item active"><a class="nav-link" href="#">Browse Products</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Compare</a></li>
+          </ul>
+        </div>
       </nav>
     </section>
   </header>
