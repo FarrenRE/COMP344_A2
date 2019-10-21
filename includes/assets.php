4ae3b3cd0ff82ab633b54c9d7f46dae2963ad2f8
FarrenRE <farren.esman@gmail.com>
2019-10-21 21:40:58 +1100

    design replica

---
+++ includes/assets.php
@@ -0,0 +1,8 @@
+  <!-- jQuery -->
+  <script src="lib/jquery-3.4.1.min.js" type="text/javascript"></script>
+  <!-- Bootstrap -->
+  <link href="lib/bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <script src="lib/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+
+  <!-- CSS -->
+  <link href="./styles.css" rel="stylesheet" type="text/css" />
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Product Comparison Page</title>
+
+<?php include 'includes/assets.php'?>
+</head>
+
+<body>
+  <header>
+    <nav class="navbar">
+      <ul>
+        <li><a href="#">Hello world</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section class="section" id="products">
+      <div class="cards">
+
+        <div class="card-wrapper">
+          <div class="card">
+            <img class="card-img-top" src="https://via.placeholder.com/350x150" alt="product 1" />
+            <div class="card-body">
+              <h3 class="card-title">Cup Brothers</h3>
+              <p class="card-text">Attributes</p>
+              <ul>
+                <li>Made from cups</li>
+                <li>Drinks and drives</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-wrapper">
+          <div class="card">
+            <img class="card-img-top" src="https://via.placeholder.com/350x150" alt="product 1" />
+            <div class="card-body">
+              <h3 class="card-title">Plumber Joe</h3>
+              <p class="card-text">Attributes</p>
+              <ul>
+                <li>Can clean well</li>
+                <li>Nice shoes</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-wrapper">
+          <div class="card">
+            <img class="card-img-top" src="https://via.placeholder.com/350x150" alt="product 1" />
+            <div class="card-body">
+              <h3 class="card-title">Dizzy Hedgehog</h3>
+              <p class="card-text">Attributes</p>
+              <ul>
+                <li>Can run well</li>
+                <li>Clean gloves</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </main>
+  <footer class="footer">
+    <div class="footer-text">COMP344 Group X</div>
+  </footer>
+</body>
+
+</html>
