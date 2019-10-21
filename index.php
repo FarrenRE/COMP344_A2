3a65adb9a32637ed622f7b0f7540a4d843107b32
FarrenRE <farren.esman@gmail.com>
2019-10-21 22:16:05 +1100

    add bootstrap classes, table, nav

--- index.php
+++ index.php
@@ -5,19 +5,24 @@
   <meta charset="utf-8" />
   <title>Product Comparison Page</title>
 
-<?php include 'includes/assets.php'?>
+  <?php include 'includes/assets.php' ?>
 </head>
 
 <body>
-  <header>
-    <nav class="navbar">
-      <ul>
-        <li><a href="#">Hello world</a></li>
-      </ul>
-    </nav>
+  <header class="bg-dark">
+    <section class="container">
+      <nav class="navbar navbar-dark header-nav">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+          <li class="nav-item active"><a class="nav-link" href="#">Browse Products</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Compare</a></li>
+        </ul>
+      </nav>
+    </section>
   </header>
   <main>
-    <section class="section" id="products">
+    <section class="container" id="products">
+      <h1 class="text-center">Products</h1>
       <div class="cards">
 
         <div class="card-wrapper">
@@ -25,11 +30,31 @@
             <img class="card-img-top" src="https://via.placeholder.com/350x150" alt="product 1" />
             <div class="card-body">
               <h3 class="card-title">Cup Brothers</h3>
-              <p class="card-text">Attributes</p>
-              <ul>
-                <li>Made from cups</li>
-                <li>Drinks and drives</li>
-              </ul>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Attribute</th>
+                    <th scope="col">Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Price</td>
+                    <td>$12.00</td>
+                  </tr>
+                  <tr>
+                    <td>Weight</td>
+                    <td>4.5 kg</td>
+                  </tr>
+                  <tr>
+                    <td>Volume</td>
+                    <td>18 db</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="card-button">
+                <a class="button button--cta" href="#">Compare me!</a>
+              </div>
             </div>
           </div>
         </div>
@@ -39,11 +64,31 @@
             <img class="card-img-top" src="https://via.placeholder.com/350x150" alt="product 1" />
             <div class="card-body">
               <h3 class="card-title">Plumber Joe</h3>
-              <p class="card-text">Attributes</p>
-              <ul>
-                <li>Can clean well</li>
-                <li>Nice shoes</li>
-              </ul>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Attribute</th>
+                    <th scope="col">Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Price</td>
+                    <td>$12.00</td>
+                  </tr>
+                  <tr>
+                    <td>Weight</td>
+                    <td>4.5 kg</td>
+                  </tr>
+                  <tr>
+                    <td>Volume</td>
+                    <td>18 db</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="card-button">
+                <a class="button button--cta" href="#">Compare me!</a>
+              </div>
             </div>
           </div>
         </div>
@@ -53,11 +98,31 @@
             <img class="card-img-top" src="https://via.placeholder.com/350x150" alt="product 1" />
             <div class="card-body">
               <h3 class="card-title">Dizzy Hedgehog</h3>
-              <p class="card-text">Attributes</p>
-              <ul>
-                <li>Can run well</li>
-                <li>Clean gloves</li>
-              </ul>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Attribute</th>
+                    <th scope="col">Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Price</td>
+                    <td>$12.00</td>
+                  </tr>
+                  <tr>
+                    <td>Weight</td>
+                    <td>4.5 kg</td>
+                  </tr>
+                  <tr>
+                    <td>Volume</td>
+                    <td>18 db</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="card-button">
+                <a class="button button--cta" href="#">Compare me!</a>
+              </div>
             </div>
           </div>
         </div>
@@ -65,9 +130,7 @@
       </div>
     </section>
   </main>
-  <footer class="footer">
-    <div class="footer-text">COMP344 Group X</div>
-  </footer>
+  <?php include 'includes/footer.php' ?>
 </body>
 
 </html>
