c50a847dd36fc7b05ec60bc1470b3bf28dbabf9b
FarrenRE <farren.esman@gmail.com>
2019-10-31 18:28:58 +1100

    place php function in functions.php

--- index.php
+++ index.php
@@ -6,32 +6,11 @@
   <title>Product Comparison Page</title>
 
   <?php
-  include 'controller.php';
+  include 'functions.php';
   include 'includes/assets.php';
   include 'includes/header.html';
   ?>
 
-  <?php
-  # render product list from database
-  function renderProductList()
-  {
-    $html = 'there is no spoon.';
-    $template = '  
-        <div class="column">
-        <div class="card">
-        <input type="checkbox" id="shirt" name="shirt">
-        <img src="' . $imgurl . '" alt="shirt" style="width:100%">
-        <h1>' . $title . '</h1>
-        <p class="price">' . $price . '</p>
-        <p>' . $desc . '</p>
-        </div>
-        </div>';
-
-    return $html;
-  }
-
-
-  ?>
 </head>
 
 <body>
